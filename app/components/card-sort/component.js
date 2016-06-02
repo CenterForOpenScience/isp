fe3bf6361f2b83319e352817851d5d9db0134b47
Saman Ehsan <se8ea@virginia.edu>
2016-06-02 17:08:14 -0400

    Add ability to drag-sort cards

--- app/components/card-sort/component.js
+++ app/components/card-sort/component.js
@@ -4,8 +4,24 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     moveButton(cards, bucket) {
-      var card = cards.shiftObject();
+      var card = cards.shiftObject(); //always move top card from the stack
       this.sendAction('moveButton', card, bucket);
+    },
+    dragCard(obj, ops) {
+      var cards = ops.target.cards;
+      var buckets = ops.target.buckets;
+
+      if (cards.contains(obj)) {
+          cards.removeObject(obj);
+      } else if (buckets.uncharacteristic.contains(obj)) {
+          buckets.uncharacteristic.removeObject(obj);
+      } else if (buckets.neutral.contains(obj)) {
+          buckets.neutral.removeObject(obj);
+      } else if (buckets.characteristic.contains(obj)){
+           buckets.characteristic.removeObject(obj);
+      }
+
+      this.sendAction('moveButton', obj, ops.target.bucket);
     }
   }
 });
