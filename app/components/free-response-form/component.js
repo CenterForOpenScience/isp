3bcd44b0393bf38305cf2a6caceff5cefeca3557
Saman Ehsan <se8ea@virginia.edu>
2016-06-02 14:21:25 -0400

    Fix JS style

--- app/components/free-response-form/component.js
+++ app/components/free-response-form/component.js
@@ -1,56 +1,56 @@
 import Ember from 'ember';
 
 const formObject = Ember.Object.create({
-    "schema": {
-        "type": "object",
-        "properties": {
-            "activity": {
-                "type": "string",
-                "title": "What were you doing yesterday at 10am/7pm?",
-                "maxLength": 75
+    schema: {
+        type: 'object',
+        properties: {
+            activity: {
+                type: 'string',
+                title: 'What were you doing yesterday at 10am/7pm?',
+                maxLength: 75
             },
-            "location": {
-                "type": "string",
-                "title": "Where were you?",
-                "maxLength": 75
+            location: {
+                type: 'string',
+                title: 'Where were you?',
+                maxLength: 75
             },
-            "peoplePresent": {
-                "type": "string",
-                "title": "Who else was present? (If you were alone, please write \"alone\").",
-                "maxLength": 75
+            peoplePresent: {
+                type: 'string',
+                title: 'Who else was present? (If you were alone, please write "alone").',
+                maxLength: 75
             }
         }
     },
-    "options": {
-        "form": {
-            "buttons": {
-            "submit": {
-                "title": "Continue",
-                "styles": "btn btn-primary"
+    options: {
+        form: {
+            buttons: {
+            submit: {
+                title: 'Continue',
+                styles: 'btn btn-primary'
             }
         }
         },
-        "fields": {
-            "activity": {
-                "type": "textarea",
-                "constrainMaxLength": true,
-                "showMaxLengthIndicator": true,
-                "validator": "required-field"
+        fields: {
+            activity: {
+                type: 'textarea',
+                constrainMaxLength: true,
+                showMaxLengthIndicator: true,
+                validator: 'required-field'
             },
-            "location": {
-                "type": "textarea",
-                "constrainMaxLength": true,
-                "showMaxLengthIndicator": true,
-                "validator": "required-field"
+            location: {
+                type: 'textarea',
+                constrainMaxLength: true,
+                showMaxLengthIndicator: true,
+                validator: 'required-field'
             },
-            "peoplePresent": {
-                "type": "textarea",
-                "constrainMaxLength": true,
-                "showMaxLengthIndicator": true,
-                "validator": "required-field"
+            peoplePresent: {
+                type: 'textarea',
+                constrainMaxLength: true,
+                showMaxLengthIndicator: true,
+                validator: 'required-field'
             }
         },
-        "focus": false
+        focus: false
     }
 });
 
