d536755bccf19bf44b1fc770ae94a3abec95c105
Saman Ehsan <se8ea@virginia.edu>
2016-06-03 14:24:30 -0400

    Shuffle list of cards

--- app/routes/survey.js
+++ app/routes/survey.js
@@ -1,17 +1,31 @@
 import Ember from 'ember';
 
+var cards = [
+ {"id": 0, "content": "Situation 0"},
+ {"id": 1, "content": "Situation 1"},
+ {"id": 2, "content": "Situation 2"},
+ {"id": 3, "content": "Situation 3"},
+ {"id": 4, "content": "Situation 4"},
+ {"id": 5, "content": "Situation 5"}
+];
+
+// h/t: http://stackoverflow.com/a/6274398
+var shuffle = function(array) {
+  let n = array.length;
+  while (n > 0) {
+      let index = Math.floor(Math.random() * n);
+      n--;  //1
+      let temp = array[n];
+      array[n] = array[index];
+      array[index] = temp;
+  }
+  return array;
+};
 
 export default Ember.Route.extend({
   model() {
    return {
-     cards: [
-       {"id": 0, "content": "Situation 0"},
-       {"id": 1, "content": "Situation 1"},
-       {"id": 2, "content": "Situation 2"},
-       {"id": 3, "content": "Situation 3"},
-       {"id": 4, "content": "Situation 4"},
-       {"id": 5, "content": "Situation 5"}
-     ],
+     cards: shuffle(cards),
      buckets: {
        uncharacteristic: [],
        neutral: [],
