4f48afb7461ab97c22de38b5388fd093882b8985
Saman Ehsan <se8ea@virginia.edu>
2016-06-06 17:17:04 -0400

    Add drag card action to second card-sort

--- app/components/card-sort-2/component.js
+++ app/components/card-sort-2/component.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    dragCard(obj, ops) {
+      var buckets = ops.target.buckets;
+      console.log(buckets);
+      for (var category in buckets) {
+        if (buckets[category].contains(obj)) {
+          buckets[category].removeObject(obj);
+        }
+      }
+
+      this.sendAction('moveButton', obj, ops.target.bucket);
+    },
+    nextSection() {
+      this.sendAction('nextSection');
+    }
+  }
 });
