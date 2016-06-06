591b04cb48a50ddccd42d9372b1df47366a80870
Saman Ehsan <se8ea@virginia.edu>
2016-06-06 09:31:54 -0400

    Remove sort buttons

--- app/components/card-sort/component.js
+++ app/components/card-sort/component.js
@@ -3,10 +3,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    moveButton(cards, bucket) {
-      var card = cards.shiftObject(); //always move top card from the stack
-      this.sendAction('moveButton', card, bucket);
-    },
     dragCard(obj, ops) {
       var cards = ops.target.cards;
       var buckets = ops.target.buckets;
