e14cf068138442fbd5888fe20cbd6ac8800e2109
Saman Ehsan <se8ea@virginia.edu>
2016-06-28 16:40:53 -0400

    Add select-input component
    Add select input form component
    since ember-bootstrap no longer
    supports its select component.

---
+++ app/components/select-input/component.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+import layout from './template';
+
+//h/t http://balinterdi.com/2015/08/29/how-to-do-a-select-dropdown-in-ember-20.html
+export default Ember.Component.extend({
+  layout,
+  options: null,
+  value: null,
+  actions: {
+    select(option){
+      this.set('value', option);
+    }
+  }
+});
