1c3af88722346f45f2581fda02ebf09f0304242e
Saman Ehsan <se8ea@virginia.edu>
2016-07-12 16:43:50 -0400

    Change i18n locale with flag-picker
    When a user selects a language from
    the flag-picker modal, set the corresponding
    i18n locale.

--- app/components/language-picker/component.js
+++ app/components/language-picker/component.js
@@ -2,25 +2,29 @@ import Ember from 'ember';
 
 import countries from './countries';
 
+
 export default Ember.Component.extend({
+    i18n: Ember.inject.service(),
     countries: countries,
     languages: Ember.computed('countries', function() {
-	var langs = [];
-	this.get('countries').forEach((country) => {
-	    country.languages.forEach((lang) => {
-		langs.push({
-		    countryCode: country.code,
-		    name: lang
-		});
-	    });
-	});
-	return langs;
+      var langs = [];
+      this.get('countries').forEach((country) => {
+        country.languages.forEach((lang) => {
+          langs.push({
+            countryCode: country.code,
+            name: lang.name,
+            code: lang.code
+          });
+        });
+      });
+      return langs;
     }),
     onPick: null,
     country: null,
     actions: {
-	pickLanguage(language) {
-	    this.get('onPick')(language);
-	}
+      pickLanguage(language, code) {
+        this.get('onPick')(language);
+        this.set('i18n.locale', code);
+      }
     }
 });
