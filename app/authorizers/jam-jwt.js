7c5f6d94acdbe3a4b356ff8915771031f5abfaaa
Saman Ehsan <se8ea@virginia.edu>
2016-08-17 17:10:03 -0400

    Remove jam-jwt authenticator & authorizer

--- app/authorizers/jam-jwt.js
+++
@@ -1,7 +0,0 @@
-import Base from 'ember-simple-auth/authorizers/base';
-
-export default Base.extend({
-  authorize(sessionData, setHeader) {
-    setHeader('Authorization', sessionData.token);
-  }
-});
