00ca25834688fd7a4ab50bf4cca11a99b9e88bfb
Saman Ehsan <se8ea@virginia.edu>
2016-09-08 13:43:38 -0400

    Put consent form text in separate file

--- app/components/isp-consent-form/component.js
+++ app/components/isp-consent-form/component.js
@@ -1,34 +1,13 @@
 import Ember from 'ember';
 import layout from './template';
-
-var content = {
-  'test': {
-    'title': 'Consent Test',
-    'paragraphs': {
-      'p1': 'Test paragraph 1',
-      'p2': 'Test is paragraph 2',
-      'p3': 'Test is paragraph 3'
-    },
-    'consentLabel': 'Test consent label.',
-    'buttonLabel': 'Test buttonLabel'
-  },
-  'test2': {
-    'title': 'Consent Test 2',
-    'paragraphs': {
-      'p1': 'Test2 paragraph 1',
-      'p2': 'Test2 paragraph 2'
-    },
-    'consentLabel': 'Test consent label 2',
-    'buttonLabel': 'Test buttonLabel 2'
-  }
-};
+import consentText from './consentText';
 
 
 export default Ember.Component.extend({
     layout: layout,
     studyId: null,
     content: Ember.computed(function() {
-      return content[this.get('studyId')];
+      return consentText[this.get('studyId')];
     }),
     consentGranted: false,
     consentNotGranted: Ember.computed.not('consentGranted')
