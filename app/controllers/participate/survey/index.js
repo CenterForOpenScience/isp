6a470e32d4101b6f3058b6123e8ee835d9510c8e
Saman Ehsan <se8ea@virginia.edu>
2016-09-15 10:24:42 -0400

    Redirect to correct page in willTransition
    When a user clicks the back-button while on the
    survey, ensure that they go back to the previous
    survey page, not the consent form (the previous
    route).

--- app/controllers/participate/survey/index.js
+++ app/controllers/participate/survey/index.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   model: null,
   session: null,
+  frameIndex: 0,
+  framePage: 1,
   store: Ember.inject.service(),
   isDirty: function() {
     // TODO: check the session model to see if it contains unsaved data
@@ -13,6 +15,14 @@ export default Ember.Controller.extend({
     saveSession(session) {
       this.set('session', session);
       this.get('session').save();
+    },
+    updateFrameIndex(frameIndex) {
+      this.set('frameIndex', frameIndex + 1);
+      // reset framePage on frame change
+      this.set('framePage', 1);
+    },
+    updateFramePage(framePage) {
+      this.set('framePage', framePage);
     }
   }
 });
--- app/routes/participate/survey/index.js
+++ app/routes/participate/survey/index.js
@@ -6,5 +6,23 @@ export default Ember.Route.extend({
     controller.set('experiment', this.controllerFor('participate.survey').get('experiment'));
     controller.set('session', session);
     controller.set('pastSessions', []);
+  },
+  actions: {
+    willTransition(transition) {
+      this._super(transition);
+      var frameIndex = this.controllerFor('participate.survey.index').get('frameIndex');
+      var framePage = this.controllerFor('participate.survey.index').get('framePage');
+      if (frameIndex !== 0) {
+        this.replaceWith('participate.survey.index');
+        // Disable back button in qsort page 2, and rating-form page 1
+        if (!(frameIndex === 2 && framePage === 2) && frameIndex !== 3) {
+          this.controllerFor('participate.survey.index').set('frameIndex', frameIndex - 1);
+        }
+        // Update pages within the rating-form
+        if (frameIndex === 3 && framePage !== 1) {
+          this.controllerFor('participate.survey.index').set('framePage', framePage - 1);
+        }
+      }
+    }
   }
 });
