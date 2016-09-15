df1923be6717d0a4b5c97e794e56b59a2f4d7599
Sam Chrisinger <s.chrisinger@gmail.com>
2016-09-15 09:53:28 -0400

    Use default 'isp' namespace

--- config/environment.js
+++ config/environment.js
@@ -41,7 +41,7 @@ module.exports = function(environment) {
     ENV.JAMDB = {
       authorizer: 'jam-jwt',
       collection:'accounts',
-      namespace: 'experimenter',
+      namespace: 'isp',
       url: 'http://localhost:1212'
     };
   }
