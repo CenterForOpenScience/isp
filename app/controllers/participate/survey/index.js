f7a0dbd186b9dcee277e50fb3bec2a6b66e76c58
Saman Ehsan <se8ea@virginia.edu>
2016-09-22 13:44:43 -0400

    Get frameIndex & framePage from session
    Use the frameIndex and/or framePage if set
    on the session so if a participant previously
    logged in and exited mid-way they would be
    returned to the last page they visited.

--- app/controllers/participate/survey/index.js
+++ app/controllers/participate/survey/index.js
@@ -3,8 +3,20 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   model: null,
   session: null,
-  frameIndex: 0,
-  framePage: 0,
+  frameIndex: Ember.computed(function() {
+    var session = this.get('session');
+    if (session && session.get('frameIndex')) {
+      return session.get('frameIndex');
+    }
+    return 0;
+  }),
+  framePage: Ember.computed(function() {
+    var session = this.get('session');
+    if (session && session.get('framePage')) {
+      return session.get('framePage');
+    }
+    return 0;
+  }),
   store: Ember.inject.service(),
   isDirty: function() {
     // TODO: check the session model to see if it contains unsaved data
