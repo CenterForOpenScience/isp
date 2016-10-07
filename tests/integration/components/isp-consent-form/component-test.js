f8aaa0cfe2735a7aa6e815387beac57f8768c9af
Saman Ehsan <se8ea@virginia.edu>
2016-10-07 11:30:26 -0400

    Add isp-consent-form test

--- tests/integration/components/isp-consent-form/component-test.js
+++ tests/integration/components/isp-consent-form/component-test.js
@@ -6,19 +6,12 @@ moduleForComponent('isp-consent-form', 'Integration | Component | isp consent fo
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{isp-consent-form}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#isp-consent-form}}
-      template block text
-    {{/isp-consent-form}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.set('studyId', 'test');
+  this.set('grantConsent', (actual) => {
+    assert.deepEqual(actual, true);
+  });
+  // Pass in hasGrantedConsent instead of setting property to override default value
+  this.render(hbs`{{isp-consent-form studyId=studyId grantConsent=(action grantConsent) hasGrantedConsent=true}}`);
+  assert.notOk((this.$('button').attr('disabled')));
+  this.$('button').click();
 });
