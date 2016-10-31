6d83553f09a0675fab725faef231adfe13baadab
Andy Boughton <abought@gmail.com>
2016-10-30 23:52:06 -0400

    Resolve deprecation warning for SafeString
    [#LEI-294]

--- app/components/progress-bar/component.js
+++ app/components/progress-bar/component.js
@@ -4,6 +4,6 @@ export default Ember.Component.extend({
   pageNumber: null,
   width: Ember.computed('pageNumber', function() {
     var width = (this.pageNumber / 11) * 100;
-    return new Ember.Handlebars.SafeString('width: ' + width + '%;');
+    return new Ember.String.htmlSafe('width: ' + width + '%;');
   })
 });
