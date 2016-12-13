6013463e58b70ce22ae0d0c30beafb09a4fddb9b
Barrett K. Harber <barrett.harber@gmail.com>
2016-12-13 14:11:35 -0500

    Remove extra from ISP session model (inherited from exp-models) Set locale from i18n.locale Dirty check: If extra attr is not set, then set it do a new object Set extra properties independently Update travis env example to reflect EXPERIMENT_ID change from STUDY_ID

--- app/models/session.js
+++ app/models/session.js
@@ -5,5 +5,4 @@ export default Session.extend({
     hasGrantedConsent: DS.attr('boolean'), // Whether user agreed to participate in the study
     frameIndex: DS.attr({defaultValue: 0}), // Index of the last visited exp-frame component
     surveyPage: DS.attr({defaultValue: 0}), // The last visited page within an exp-frame component
-    extra: DS.attr({ defaultValue: () => {} }),
 });
