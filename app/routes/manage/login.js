3cc62cb6aa562ab24f7be59e0b6e41e9e0909ad7
Andy Boughton <abought@gmail.com>
2016-12-15 10:46:59 -0500

    Entire mgmt section of site is english-only (set for parent section of router, not just login route)
    [#LEI-326]

--- app/routes/manage/login.js
+++ app/routes/manage/login.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    i18n: Ember.inject.service(),
-
-    beforeModel() {
-        this.set('i18n.locale', 'en-us');
-        return this._super(...arguments);
-    }
 });
