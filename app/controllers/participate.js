c25c3f5145c55ef735e0f64471a1058e10608ddb
Andy Boughton <abought@gmail.com>
2017-01-06 14:02:14 -0500

    Simplify isRTL check by reusing ember-i18n functions
    [ci skip]
    [#LEI-348]

--- app/controllers/participate.js
+++ app/controllers/participate.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { walkConfigs } from '../utils/locale-utils';
 
 export default Ember.Controller.extend({
     i18n: Ember.inject.service(),
@@ -6,19 +7,12 @@ export default Ember.Controller.extend({
     // Set by login page
     locale: null,
 
-    rtl: Ember.computed('i18n.locale', function() {
-        // Big assumption: any RTL locale we include MUST define the config manually! (can't seem to load the ember-18n config)
-
+    isRTL: Ember.computed('i18n.locale', function() {
         const locale = this.get('i18n.locale');
-        // TODO undefined check
-        const baseLocale = locale.split('-')[0];
-        // Ember i18n service doesn't expose config directly, so we fetch it in a manner analogous to `addTranslations`
-        // https://github.com/jamesarosen/ember-i18n/issues/375
-        // https://github.com/jamesarosen/ember-i18n/blob/a98bdaecebb0cddfa30ef98aacc032f38ac50ae5/addon/utils/add-translations.js#L6
-        const key = `locale:${baseLocale}/config`;  // TODO: check base translation code- this only works if locales explicitly define a config
-        const config = Ember.getOwner(this)._lookupFactory(key);
-        console.log('Found config!', config, config.rtl);
-        return config.rtl;
+        const config = walkConfigs(locale, Ember.getOwner(this)) || {};
+
+        // If no setting defined, default to rtl false (consistent with ember-i18n defaultConfig)
+        return !!config.rtl;
     }),
 
     init() {
