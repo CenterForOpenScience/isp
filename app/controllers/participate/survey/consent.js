2605427c1c36bcdd10de75be8f7c10949dfe3d9a
Andy Boughton <abought@gmail.com>
2017-01-12 13:57:50 -0500

    Move rtl logic to application route so all pages obey (including login screen)
    [ci skip]
    [#LEI-350]

--- app/controllers/participate/survey/consent.js
+++ app/controllers/participate/survey/consent.js
@@ -1,17 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    i18n: Ember.inject.service(),
 
     session: null,
 
     studyId: Ember.computed(function () {
         return Ember.getOwner(this).lookup('controller:participate').get('studyId');
-    }),
-
-    rtlDir: Ember.computed('i18n.locale', function() {
-        // Define whether the selected locale is RTL.
-        return !!this.get('i18n._locale.rtl') ? 'rtl' : 'ltr';
     }),
 
     actions: {
