fdec6fdfbbc370a1631f38c3c8652c4e66687d9e
Addison Schiller <acschiller@gmail.com>
2017-07-12 12:22:21 -0400

    scripts for drag and drop testing

---
+++ scripts/drag_scripts/drag_cat3_helper.py
@@ -0,0 +1,31 @@
+import pyautogui
+
+pyautogui.PAUSE = .06
+SPEED = .1
+positions = []
+print("If you have a dual monitor set up, make sure the browser window is located on your main monitor.\n If you are using a screen and laptop, the laptop is your main monitor.")
+print("Please align your browser so you can see all category boxes. While you input and follow these commands, do not move or scroll your browswer window!\n")
+
+input("Please put your mouse over the top box, near the middle if possible, then return.")
+center_position = pyautogui.position()
+
+input("Please mouse over the left most box, and press return.")
+positions.append(pyautogui.position())
+
+input("Please mouse over the middle box, and press return.")
+positions.append(pyautogui.position())
+
+input("Please mouse over the right most box, and press return")
+positions.append(pyautogui.position())
+
+offsets = []
+for position in positions:
+    offset = (- center_position[0] + position[0], - center_position[1] + position[1])
+    offsets.append(offset)
+
+for cardset in range(30):  # cardset is just the next 3 cards, eg, each category gets a card, then do the next set, for 90 in total.
+    for offset in offsets:
+        pyautogui.moveTo(center_position[0], center_position[1], SPEED)
+        pyautogui.click()
+        pyautogui.dragRel(offset[0], offset[1], SPEED, button='left')
+        pyautogui.click()
