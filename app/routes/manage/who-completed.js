8a4b09e08f6dc6cfd5a3dd28fe366a1d31b5f307
Addison Schiller <acschiller@gmail.com>
2017-07-18 15:08:04 -0400

    Avoid transition errors when changing query parameters
     * Override the default queryParamsDidChange[1] method to stop certain
       page refeshes from going through and causing promises to be
       rejected. Certain actions would trigger the function multiple
       times. Adding the Ember.run.once() function around it stops this
       from happening.  Setting refreshModel to true under queryParams is
       what is calling this function.
    
       See https://github.com/emberjs/ember.js/issues/5566 for more info
       on the issue and more possible fixes.
    
       This fix could possibly change some interactions on this route in
       the future (e.g. if more queryParams are added). If the old
       function is desired, comment out the new one, and ember will
       use its default.
    
       For future reference there may be other possible fixes with
       observers that do not override the default queryParamsDidChange
       function.
    
       [1] https://github.com/emberjs/ember.js/blob/lts-2-8/packages/ember-routing/lib/system/route.js
    
       [SVCS-393]
    
       Fixes #172

--- app/routes/manage/who-completed.js
+++ app/routes/manage/who-completed.js
@@ -19,5 +19,25 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
             query['filter[extra.studyId]'] = params.siteId;
         }
         return this.store.queryEverything('account', query);
+    },
+
+    actions: {
+
+        // Override Embers queryParamsDidChange function, and make a few modifications.
+        // The initial function uses 'this.refresh' which will call multiple times, causing transition aborts
+        // Replacing with nesting the call in a Ember.run.once fixes this
+        queryParamsDidChange(changed, totalPresent, removed){
+            //queryParams map
+            var qpMap = this.get('_qp').map;
+            var totalChanged = Object.keys(changed).concat(Object.keys(removed));
+            for (var i = 0; i < totalChanged.length; ++i){
+                var qp = qpMap[totalChanged[i]];
+                // the this.router.currentState variable causes a page refresh to fail this check. Prevents a failed promise. 
+                if (qp && this._optionsForQueryParam(qp).refreshModel && this.router.currentState) {
+                    Ember.run.once(this, this.refresh);
+                }
+            }
+            return true;
+        },
     }
 });
