e997a3556e1b9af438f9f5203d5feab491026338
Fitz Elliott <fitz@cos.io>
2017-08-08 13:00:59 -0400

    add some hesitation to cat3/9 helpers
     * Wait 3 seconds before setting off the clickstorm to allow
       fullscreen users time to switch back to their browser.
    
     * Wrap long lines, minor fixes for grammar/spelling issues.

--- scripts/drag_scripts/drag_cat3_helper.py
+++ scripts/drag_scripts/drag_cat3_helper.py
@@ -1,12 +1,22 @@
+import time
+
 import pyautogui
 
-pyautogui.PAUSE = .06
+HESITATE = 3  # time to wait before starting clickstorm
 SPEED = .1
+pyautogui.PAUSE = .06
 positions = []
-print("If you have a dual monitor set up, make sure the browser window is located on your main monitor.\n If you are using a screen and laptop, the laptop is your main monitor.")
-print("Please align your browser so you can see all category boxes. While you input and follow these commands, do not move or scroll your browswer window!\n")
 
-input("Please put your mouse over the top box, near the middle if possible, then return.")
+print("If you have a dual monitor set up, make sure the browser window is "
+      "located on your main monitor.\nIf you are using a screen and laptop, "
+      "the laptop is your main monitor.\n")
+
+print("Please align your browser so you can see all category boxes. While "
+      "you input and follow these commands, do not move or scroll your "
+      "browser window!\n")
+
+input("Please put your mouse over the top box, near the middle if possible, "
+      "then return.")
 center_position = pyautogui.position()
 
 input("Please mouse over the left most box, and press return.")
@@ -18,12 +28,19 @@ positions.append(pyautogui.position())
 input("Please mouse over the right most box, and press return")
 positions.append(pyautogui.position())
 
+print("Please switch back to your browser window. The script will begin "
+      "in {} seconds".format(HESITATE))
+time.sleep(HESITATE)
+
 offsets = []
 for position in positions:
-    offset = (- center_position[0] + position[0], - center_position[1] + position[1])
+    offset = (- center_position[0] + position[0],
+              - center_position[1] + position[1])
     offsets.append(offset)
 
-for cardset in range(30):  # cardset is just the next 3 cards, eg, each category gets a card, then do the next set, for 90 in total.
+# cardset is just the next 3 cards, eg, each category gets a card, then do the
+# next set, for 90 in total.
+for cardset in range(30):
     for offset in offsets:
         pyautogui.moveTo(center_position[0], center_position[1], SPEED)
         pyautogui.click()
--- scripts/drag_scripts/drag_cat9_helper.py
+++ scripts/drag_scripts/drag_cat9_helper.py
@@ -1,55 +1,86 @@
+import time
+
 import pyautogui
 
-pyautogui.PAUSE = .06  # can make it faster, but can cause issues with placement etc
+HESITATE = 3  # time to wait before starting clickstorm
+pyautogui.PAUSE = .06  # going faster may cause issues with placement
 SPEED = .1
+
 positions = []
-print("If you have a dual monitor set up, make sure the browser window is located on your main monitor.\n If you are using a screen and laptop, the laptop is your main monitor.")
-print("Please align your browser so you can see all category boxes. While you input and follow these commands, do not move or scroll your browswer window!")
-input("Please mouse over the top most draggable option in the left most category box. It should be labeled with 'Uncharacteristic'. Then press return.")
+
+print("If you have a dual monitor set up, make sure the browser window is "
+      "located on your main monitor.\n If you are using a screen and laptop, "
+      "the laptop is your main monitor.")
+
+print("Please align your browser so you can see all category boxes. While you "
+      "input and follow these commands, do not move or scroll your browswer "
+      "window!")
+
+input("Please mouse over the top most draggable option in the leftmost "
+      "category box. It should be labeled with 'Uncharacteristic'. Then press "
+      "return.")
 left_position = pyautogui.position()
 
-input("Please mouse over the top most draggable option in the middle category box. It should be labeled with 'Neutral'. Then press return.")
+input("Please mouse over the top most draggable option in the middle category "
+      "box. It should be labeled with 'Neutral'. Then press return.")
 center_position = pyautogui.position()
 
-input("Please mouse over the top most draggable option in the right most category box. It should be labeled with 'Characteristic'. Then press return.")
+input("Please mouse over the top most draggable option in the rightmost "
+      "category box. It should be labeled with 'Characteristic'. Then press "
+      "return.")
 right_position = pyautogui.position()
 
-print("Now, for each of the bottom category boxes, mouse over them and press return, then move onto the next box. There should be 9 boxes in total.")
+print("Now, for each of the bottom category boxes, mouse over them and press "
+      "return, then move onto the next box. There should be 9 boxes in total.")
 for categorynum in range(9):
     input(categorynum)
     positions.append(pyautogui.position())
 
+print("Please switch back to your browser window. The script will begin "
+      "in {} seconds".format(HESITATE))
+time.sleep(HESITATE)
 
-# the below counts in the array smy look odd, but it was just the easiest way to break the containers. Since the first one needs 3 in it, second needs 6 in it etc..
-# it adds up to 30 because at the end of running the script for cat 3, each container will have 30 in it. In the middle part, the 5 is there because that container has
-# 5 slots in it left over after 10 are placed in it from the first loop.
-# The general idea is, starting on the left, place cards into the containers till full,
-# then move onto the next container. if a container fills up or contains no more cards, move onto the next.
+# The below counts in the array may look odd, but it was just the
+# easiest way to break the containers. Since the first one needs 3 in
+# it, second needs 6 in it etc. it adds up to 30 because at the end
+# of running the script for cat 3, each container will have 30 in
+# it. In the middle part, the 5 is there because that container has 5
+# slots in it left over after 10 are placed in it from the first loop.
+# The general idea is, starting on the left, place cards into the
+# containers till full, then move onto the next container. if a
+# container fills up or contains no more cards, move onto the next.
 
-# the first array in the zip corresponds to the amount that needs to go in the below containers on cat9,
-# and adds up the 30. the range(4) is there to grab the appropriate offset for containers 1-4 etc.
+# The first array in the zip corresponds to the amount that needs to
+# go in the below containers on cat9, and adds up the 30. The range(4)
+# is there to grab the appropriate offset for containers 1-4 etc.
 for card_count, pos_count in zip([3, 6, 11, 10], range(4)):
-    offset = (- left_position[0] + positions[pos_count][0], - left_position[1] + positions[pos_count][1])
+    offset = (- left_position[0] + positions[pos_count][0],
+              - left_position[1] + positions[pos_count][1])
     for k in range(card_count):
         pyautogui.moveTo(left_position[0], left_position[1], SPEED)
         pyautogui.click()
         pyautogui.dragRel(offset[0], offset[1], SPEED, button='left')
         pyautogui.click()
 
-# the first array in the zip corresponds to the amount that needs to go in the below containers on cat9, and adds up the 30.
-# the range(3,6) is there to grab the appropriate offset for containers 3-6 etc.
+# The first array in the zip corresponds to the amount that needs to
+# go in the below containers on cat9, and adds up the 30.  The
+# range(3,6) is there to grab the appropriate offset for containers
+# 3-6 etc.
 for card_count, pos_count in zip([5, 20, 5], range(3, 6)):
-    offset = (- center_position[0] + positions[pos_count][0], - center_position[1] + positions[pos_count][1])
+    offset = (- center_position[0] + positions[pos_count][0],
+              - center_position[1] + positions[pos_count][1])
     for k in range(card_count):
         pyautogui.moveTo(center_position[0], center_position[1], SPEED)
         pyautogui.click()
         pyautogui.dragRel(offset[0], offset[1], SPEED, button='left')
         pyautogui.click()
 
-# the first array in the zip corresponds to the amount that needs to go in the below containers on cat9,
-# and adds up the 30. the range(4) is there to grab the appropriate offset for containers 5-6 etc.
+# The first array in the zip corresponds to the amount that needs to
+# go in the below containers on cat9, and adds up the 30. The range(4)
+# is there to grab the appropriate offset for containers 5-6 etc.
 for card_count, pos_count in zip([10, 11, 6, 3], range(5, 9)):
-    offset = (- right_position[0] + positions[pos_count][0], - right_position[1] + positions[pos_count][1])
+    offset = (- right_position[0] + positions[pos_count][0],
+              - right_position[1] + positions[pos_count][1])
     for k in range(card_count):
         pyautogui.moveTo(right_position[0], right_position[1], SPEED)
         pyautogui.click()
