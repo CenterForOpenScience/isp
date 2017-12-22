ac2e4a0d322c287466de229837b1d5959f912e84
Fitz Elliott <fitz@cos.io>
2017-12-22 12:38:44 -0500

    further corrections for finnish translation

--- app/locales/fi/translations.js
+++ app/locales/fi/translations.js
@@ -194,14 +194,14 @@ export default {
                 "label": "Oliko kuvailemasi tilanne kokonaisuudessaan myönteinen vai kielteinen kokemus?",
                 "options": {
                     "extremelyNeg": "Erittäin kielteinen",
-                    "extremelyPos": "Erittäin kielteinen",
+                    "extremelyPos": "Erittäin myönteinen",
                     "fairlyNeg": "Melko kielteinen",
-                    "fairlyPos": "Melko kielteinen",
+                    "fairlyPos": "Melko myönteinen",
                     "neither": "Ei myönteinen eikä kielteinen",
                     "quiteNeg": "Hyvin kielteinen",
-                    "quitePos": "Hyvin kielteinen",
+                    "quitePos": "Hyvin myönteinen",
                     "somewhatNeg": "Jokseenkin kielteinen",
-                    "somewhatPos": "Jokseenkin kielteinen"
+                    "somewhatPos": "Jokseenkin myönteinen"
                 }
             },
             "10": {
